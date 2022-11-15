8cd01fd4c00405e2b8581bf58a7bf3a5916a2ee7
Natalia <natirgh@gmail.com>
2022-11-15 17:28:46 +0100

    remove the now ignored dist and coverage directory

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jest_config_1 = require("jest-config");
-const config = {
-    moduleFileExtensions: [...jest_config_1.defaults.moduleFileExtensions, "mts"],
-    transform: {},
-    testEnvironment: "jest-environment-node",
-};
-exports.default = config;
